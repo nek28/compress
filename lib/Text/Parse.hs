45a6c6ba9dc954ca7d4fad92892bbd34368dab43
nek28 <vladinekolov@gmail.com>
2019-02-28 00:06:24 +0200

    add more parser functions in lib

--- lib/Text/Parse.hs
+++ lib/Text/Parse.hs
@@ -7,7 +7,9 @@ import qualified Data.Text as T
 newtype Parser a = Parser { parse :: Text -> [(a,Text)] }
 
 item :: Parser Char
-item = Parser $ \inp -> [(T.head inp, T.tail inp)]
+item = Parser $ \inp -> if inp == T.empty
+                        then []
+                        else [(T.head inp, T.tail inp)]
 
 (|-) :: Parser a -> (a -> Parser b) -> Parser b
 pA |- fTopB = Parser $ \inp -> concatMap (\(result, rest) -> parse (fTopB result) rest) (parse pA inp)
@@ -46,11 +48,33 @@ upper = sat (\c -> c >= 'A' && c <= 'Z')
 letter = lower <|> upper
 alphanum = letter <|> digit
 
+nat :: Parser Int
+nat = manyText digit |- (result . eval)
+    where
+        eval :: Text -> Int
+        eval = T.foldl' (\base num -> (base * 10) + (toInt num)) 0
+        toInt c = fromEnum c - fromEnum '0'
+
+int :: Parser Int
+int = neg |- (\f ->
+            nat |- (\n ->
+                result $ f n))
+    where
+        neg = minus <|> result id
+        minus = char '-' |- (\_ -> result (negate))
+
 word = plusOneLetter <|> result T.empty
     where
         plusOneLetter = letter |- (\l ->
                                 word |- (\rest ->
                                         result (T.cons l rest)))
+
+manyText :: Parser Char -> Parser Text
+manyText pC = oneCharFurther <|> result T.empty
+    where
+        oneCharFurther = pC |- (\firstChar ->
+                                manyText pC |- (\rest ->
+                                        result $ T.cons firstChar rest))
 
 many :: Parser a -> Parser [a]
 many pA = oneStepFurther <|> result []
@@ -68,4 +92,7 @@ text t = if t == T.empty
                             text (T.tail t) |- (\rest ->
                                     result $ T.cons first rest))
 
-
+sepby1 :: Parser a -> Parser b -> Parser [a]
+sepby1 pA pSep = pA |- (\first ->
+                        many (pSep |- (\_ -> pA |- result)) |- (\rest ->
+                                result $ first : rest))
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import Text.Parse
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
